7e1eae7dbc40cd20d15bf3e1f64909326f70409c
jahnavijanu123 <jahnavidronadula@gmail.com>
2024-12-29 15:35:39 +0530

    reactupdate

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Day3 from './tasks/day3';
+// import Day4 from './tasks/day4';
+import Day5 from './tasks/day5';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  {/* <Day1/> */}
+  {/* <Day3/> */}
+  {/* <Day4/> */}
+  {/* <Day5/> */}
+
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
